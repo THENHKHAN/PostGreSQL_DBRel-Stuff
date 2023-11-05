846efbd616ea33cb12873669a0336ced46c4ceb3
Noorul Huda Khan <92791314+THENHKHAN@users.noreply.github.com>
2023-11-05 14:18:02 +0530

    02:26PM-05Nov2023-FromQ7-7Done From crtTble Exer
    PostgreSQL **Create Table** Exercise is going on

--- 2_PracticeFromW3Resource/1_CreateTableExercise.sql
+++ 2_PracticeFromW3Resource/1_CreateTableExercise.sql
@@ -49,11 +49,43 @@ CREATE TABLE dup_countries
 	
 /* 5. Write a SQL statement to create a table countries set a constraint NULL.
 */
-DROP TABLE countries;
+DROP TABLE countries; -- so that i can create below without updating schema.
 CREATE TABLE IF NOT EXISTS countries (
 
 		country_id  VARCHAR(3) NOT NULL,
 		country_name VARCHAR(40) NOT NULL,
 		region_id DECIMAL(10,0) NOT NULL
 );
+
+/* 6. Write a SQL statement to create a table named jobs including columns job_id, job_title, min_salary, max_salary and 
+check whether the max_salary amount exceeding the upper limit 25000.
+*/
+
+CREATE TABLE IF NOT EXISTS jobs (
+		job_id varchar(10) NOT NULL ,
+		job_title varchar(35) NOT NULL,
+		min_salary DECIMAL(6,0) ,
+		max_salary Decimal(6,0)
+	-- this is how to use CHECK constraint 
+	CHECK (max_salary <= 25000) -- CHECK allows you to specify a condition or expression that the data in a particular column or combination of columns must adhere to.
+);
+-- If a row is inserted or updated in the jobs table, and the condition (max_salary <= 25000) is not met (i.e., if the max_salary exceeds 25,000), 
+-- it will result in a constraint violation, and the operation will be rejected.
+-- In this case, the CHECK constraint is used to enforce a business rule that limits the max_salary values to a specific maximum amount (25,000). 
+-- It helps ensure data integrity by preventing invalid data from being inserted into the table.
+SELECT * FROM jobs;	
+
+/* 7. Write a SQL statement to create a table named countries, including country_id, country_name and region_id and make sure that no countries except Italy, 
+		India and China will be entered in the table.
+*/
+drop TABLE countries;
+-- so in this also we have make a check but will multiple option so need to use IN operator
+CREATE TABLE IF NOT EXISTS countries (
 		
+		country_id  VARCHAR(3) NOT NULL,
+		country_name VARCHAR(40) NOT NULL,
+		region_id DECIMAL(10,0) NOT NULL
+		CHECK (country_name IN ('Italy' , 'India' , 'China')) --OR more readable
+-- 		CONSTRAINT valid_countries CHECK (country_name IN ('Italy' , 'India' , 'China')) -- valid_countries - this will be the constraint name
+)
+SELECT * FROM countries;
